fa85b4493df2bb542b997e6dd9d0e562ed49621f
Ankit Mishra <ankit1703@gmail.com>
2022-04-18 08:18:08 +0530

    Head First Go Code Snippets

---
+++ Chapter_01/Hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Hello, Go!")
+	fmt.Println("First Argument", "Second Argument")
+	fmt.Println("Another line")
+	fmt.Println(math.Floor(2.75))
+	fmt.Println(strings.Title("head first go"))
+}
---
+++ Chapter_01/Strings.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, \nGo!")
+	fmt.Println("Hello, \tGo!")
+	fmt.Println("Quotes, \"\" ")
+	fmt.Println("Backslash: \\")
+}
